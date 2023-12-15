57e6695a7d2bcdcd03dc84f8e68ac0e111a02ab3
fdkevin <fdkevinzero@gmail.com>
2023-12-15 17:25:31 +0800

    feat: use from_int

--- core/src/stream.rs
+++ core/src/stream.rs
@@ -1,62 +1,61 @@
 use ceramic_core::{Cid, StreamId};
 use chrono::{DateTime, Utc};
-use dataverse_ceramic::{
-    event::{self, Event},
-    StreamState,
-};
+use dataverse_ceramic::event::{Event, EventValue};
+use dataverse_ceramic::StreamState;
+use int_enum::IntEnum;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Stream {
-    pub r#type: u64,
-    pub dapp_id: uuid::Uuid,
-    // pub network: String,
-    pub expiration_time: Option<DateTime<Utc>>,
-    pub genesis: Cid,
-    pub tip: Cid,
-    pub model: Option<StreamId>,
-    pub published: usize,
+	pub r#type: u64,
+	pub dapp_id: uuid::Uuid,
+	// pub network: String,
+	pub expiration_time: Option<DateTime<Utc>>,
+	pub genesis: Cid,
+	pub tip: Cid,
+	pub model: Option<StreamId>,
+	pub published: usize,
 }
 
 impl Stream {
-    pub fn new(
-        dapp_id: &uuid::Uuid,
-        r#type: u64,
-        genesis: &event::Event,
-        model: Option<StreamId>,
-    ) -> anyhow::Result<Self> {
-        if let event::EventValue::Signed(signed) = &genesis.value {
-            let expiration_time = match signed.cacao()? {
-                Some(cacao) => cacao.p.expiration_time()?,
-                None => None,
-            };
-            return Ok(Stream {
-                r#type,
-                dapp_id: dapp_id.clone(),
-                expiration_time,
-                published: 0,
-                tip: genesis.cid,
-                genesis: genesis.cid,
-                model: model,
-            });
-        }
-        anyhow::bail!("invalid genesis commit");
-    }
+	pub fn new(
+		dapp_id: &uuid::Uuid,
+		r#type: u64,
+		genesis: &Event,
+		model: Option<StreamId>,
+	) -> anyhow::Result<Self> {
+		if let EventValue::Signed(signed) = &genesis.value {
+			let expiration_time = match signed.cacao()? {
+				Some(cacao) => cacao.p.expiration_time()?,
+				None => None,
+			};
+			return Ok(Stream {
+				r#type,
+				dapp_id: dapp_id.clone(),
+				expiration_time,
+				published: 0,
+				tip: genesis.cid,
+				genesis: genesis.cid,
+				model: model,
+			});
+		}
+		anyhow::bail!("invalid genesis commit");
+	}
 
-    pub fn stream_id(&self) -> anyhow::Result<StreamId> {
-        Ok(StreamId {
-            r#type: self.r#type.try_into()?,
-            cid: self.genesis,
-        })
-    }
+	pub fn stream_id(&self) -> anyhow::Result<StreamId> {
+		Ok(StreamId {
+			r#type: IntEnum::from_int(self.r#type)?,
+			cid: self.genesis,
+		})
+	}
 
-    pub async fn state(&self, commits: Vec<Event>) -> anyhow::Result<StreamState> {
-        StreamState::make(self.r#type, commits).await
-    }
+	pub async fn state(&self, commits: Vec<Event>) -> anyhow::Result<StreamState> {
+		StreamState::make(self.r#type, commits).await
+	}
 }
 
 #[async_trait::async_trait]
 pub trait StreamStore {
-    async fn save_stream(&self, stream: &Stream) -> anyhow::Result<()>;
-    async fn load_stream(&self, stream_id: &StreamId) -> anyhow::Result<Option<Stream>>;
+	async fn save_stream(&self, stream: &Stream) -> anyhow::Result<()>;
+	async fn load_stream(&self, stream_id: &StreamId) -> anyhow::Result<Option<Stream>>;
 }
